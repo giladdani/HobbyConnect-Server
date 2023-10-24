f45ac21b9d603c60c67aedfe53463522125e7d06
giladdanino <daninog6@gmail.com>
2023-10-24 15:05:04 +0300

    removed blank newline for testing

--- routes/router.ts
+++ routes/router.ts
@@ -8,7 +8,6 @@ import activities from './activities.ts'
 // @ts-ignore
 
 let router = Router();
-
 router.use('/users', users);
 router.use('/giftcodes', giftcodes);
 router.use('/activities', activities);
